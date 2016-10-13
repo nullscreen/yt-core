65b8a46af8a68bdb768492eceb753bfe46809c4e
claudiob <claudiob@inventati.org>
2016-10-12 17:07:39 -0700

    Add Yt::Errors::NoItems

---
+++ lib/yt/errors/no_items.rb
@@ -0,0 +1,10 @@
+module Yt
+  module Errors
+    # Raised when items are expected from YouTube but none are returned.
+    # @example Fetch the title of a channel with an unknown ID.
+    #   Yt::Models::Channel.new(id: 'unknown-id').title
+    #   # => raise Yt::Errors::NoItems
+    class NoItems < StandardError
+    end
+  end
+end
