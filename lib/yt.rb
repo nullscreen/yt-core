0573e7815009e6892aae6e657a102bb422f7194a
claudiob <claudiob@inventati.org>
2016-10-19 11:55:35 -0700

    Add Channel#status and Channel#select

--- lib/yt.rb
+++ lib/yt.rb
@@ -1,6 +1,9 @@
 require 'active_support' # does not load anything by default
 require 'active_support/core_ext/object/to_query' # for Hash.to_param
 require 'active_support/core_ext/hash/transform_values' # for Hash#transform_values
+require 'active_support/core_ext/object/blank' # for Object#presence
+require 'active_support/hash_with_indifferent_access' # for HashWithIndifferentAccess#new
+require 'active_support/core_ext/hash/indifferent_access' # for a bug in Rails
 
 require 'json' # for JSON.parse
 
